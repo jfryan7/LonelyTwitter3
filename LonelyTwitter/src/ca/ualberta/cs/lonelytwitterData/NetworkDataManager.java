516087edae003d9e51e8d7e3a2d41da95a87552c
jfryan7 <jfryan@ualbeta.ca>
2014-09-17 19:23:25 -0600

    Lab finished

---
+++ LonelyTwitter/src/ca/ualberta/cs/lonelytwitterData/NetworkDataManager.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitterData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.ualberta.cs.lonelytwitter.AbstractTweet;
+
+
+public class NetworkDataManager implements IDataManager{
+
+	public ArrayList<AbstractTweet> loadTweets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public void saveTweets(List<AbstractTweet> lts) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
